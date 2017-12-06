23055317c54dfd7e58e248ca8a159f18b187c559
Ayman Boustati <ayman.boustati@outlook.com>
2017-12-06 11:32:50 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dgplib',
+      version='0.1dev',
+      author='Ayman Boustati',
+      author_email='aboustati@turing.ac.uk',
+      license='Apache License 2.0',
+      packages=['dgplib'],
+      test_suite='tests.test_module_suite'
+     )
