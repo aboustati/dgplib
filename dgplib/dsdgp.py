1e99f1e7dab4d2f87122f56b52321d3d8f12137b
Ayman Boustati <ayman.boustati@outlook.com>
2017-12-29 20:29:15 +0000

    added methods to DSDGP object

--- dgplib/dsdgp.py
+++ dgplib/dsdgp.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 from gpflow import settings
 
-from gpflow.decors import defer_build
+from gpflow.decors import autoflow, defer_build
 from gpflow.mean_functions import Zero
 from gpflow.model import Model
 from gpflow.params import DataHolder, Minibatch
@@ -72,3 +72,78 @@ class DSDGP(Model):
             Y = Minibatch(Y, batch_size=minibatch_size, seed=0)
 
         self.X, self.Y = X, Y
+
+    #Credits to gpflow dev team
+    @autoflow((settings.float_type, [None, None]), (tf.int32, []))
+    def predict_f(self, Xnew, num_samples):
+        """
+        Compute the mean and variance of the latent function(s) for the final
+        layer at the points Xnew.
+        """
+        return self._build_predict(Xnew, num_samples)
+
+    #Credits to gpflow dev team
+    @autoflow((settings.float_type, [None, None]))
+    def predict_f_full_cov(self, Xnew):
+        """
+        Compute the mean and covariance matrix of the latent function(s) for
+        the final layer at the points Xnew.
+        """
+        return self._build_predict(Xnew, full_cov=True)
+
+
+    #Credits to Hugh Salimbeni
+    @autoflow((settings.float_type, [None, None]), (tf.int32, []))
+    def predict_all_layers(self, Xnew, num_samples):
+        """
+        Compute the mean and variance of the latent function(s) for for all
+        layers at the points Xnew.
+        """
+        return self.propagate(Xnew, num_samples)
+
+    #Credits to Hugh Salimbeni
+    @autoflow((settings.float_type, [None, None]))
+    def predict_all_layers_full_cov(self, Xnew):
+        """
+        Compute the mean and covariance matrix of the latent function(s) for
+        all layers at the points Xnew.
+        """
+        return self.propagate(Xnew, full_cov=True)
+
+    #Credits to gpflow dev team
+    @autoflow((settings.float_type, [None, None]), (tf.int32, []))
+    def predict_f_samples(self, Xnew, num_samples):
+        """
+        Produce samples from the posterior latent function(s) for the final
+        layer at the points Xnew.
+        """
+        mu, var = self._build_predict(Xnew, full_cov=True)
+        jitter = tf.eye(tf.shape(mu)[0], dtype=settings.float_type) * settings.numerics.jitter_level
+        samples = []
+        for i in range(self.num_latent):
+            L = tf.cholesky(var[:, :, i] + jitter)
+            shape = tf.stack([tf.shape(L)[0], num_samples])
+            V = tf.random_normal(shape, dtype=settings.float_type)
+            samples.append(mu[:, i:i + 1] + tf.matmul(L, V))
+        return tf.transpose(tf.stack(samples))
+
+    #Credits to gpflow dev team
+    @autoflow((settings.float_type, [None, None]))
+    def predict_y(self, Xnew):
+        """
+        Compute the mean and variance of held-out data at the points Xnew
+        """
+        pred_f_mean, pred_f_var = self._build_predict(Xnew)
+        return self.likelihood.predict_mean_and_var(pred_f_mean, pred_f_var)
+
+    #Credits to gpflow dev team
+    # @autoflow((settings.float_type, [None, None]), (settings.float_type, [None, None]))
+    # def predict_density(self, Xnew, Ynew):
+        # """
+        # Compute the (log) density of the data Ynew at the points Xnew
+        # Note that this computes the log density of the data individually,
+        # ignoring correlations between them. The result is a matrix the same
+        # shape as Ynew containing the log densities.
+        # """
+        # pred_f_mean, pred_f_var = self._build_predict(Xnew)
+        # return self.likelihood.predict_density(pred_f_mean, pred_f_var, Ynew)
