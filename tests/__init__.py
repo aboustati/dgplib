a676d493f1129294a6f0eae326a693bf7e71c0b7
Ayman Boustati <ayman.boustati@outlook.com>
2017-12-30 19:58:28 +0000

    enabled test discovery

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,10 @@
 import unittest
 
 from . import test_layer
+from . import test_models
 
 def test_module_suite():
     loader = unittest.TestLoader()
-    suite = loader.loadTestsFromModule(test_layer)
+    #suite = loader.loadTestsFromModule(test_layer)
+    suite = loader.discover('./')
     return suite
