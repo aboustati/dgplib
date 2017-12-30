b6b43cfdf4bf3c9e0a839a02918c5a76ef48e9a1
Ayman Boustati <ayman.boustati@outlook.com>
2017-12-30 19:57:17 +0000

    Improved assertions in Sequential

--- dgplib/models.py
+++ dgplib/models.py
@@ -13,11 +13,17 @@ class Sequential(Parameterized):
         """
         - layers is a list of layer objects.
         """
+
+        super(Sequential, self).__init__(name=name)
+
         self.layers = ParamList([])
 
         if layers:
-            assert isinstance(layers[0], InputLayer)
-            assert isinstance(layers[-1], OutputLayer)
+            assert isinstance(layers[0], InputLayer), """First layer must be an
+            instance of InputLayer"""
+            assert isinstance(layers[-1], OutputLayer), """Final layer must be an
+            instance of OutputLayer"""
+
             for layer in layers:
                 self.add(layer)
 
@@ -27,12 +33,18 @@ class Sequential(Parameterized):
 
         - layer is an instance of an object that inherits from Layer
         """
-        assert issubclass(layer, Layer)
+        assert isinstance(layer, Layer)
+
+        if isinstance(self.layers[-1], OutputLayer):
+            raise ValueError('Cannot add layers after an Output Layer')
 
         if not self.layers:
-            assert isinstance(layer, InputLayer)
+            assert isinstance(layer, InputLayer), """First layer must be an
+            Input Layer"""
         else:
-            assert self.layers[-1].output_dim == layer.input_dim
+            assert self.layers[-1].output_dim == layer.input_dim, """Input
+            dimensions of layer must be equal to the output dimensions of the
+            preceding layer"""
 
         self.layers.append(layer)
 
