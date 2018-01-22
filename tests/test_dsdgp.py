03a15925c3485f9d8ae156119f1328e2d58aac89
Ayman Boustati <ayman.boustati@outlook.com>
2018-01-22 19:18:47 +0000

    added tests for DSDGP methods

--- tests/test_dsdgp.py
+++ tests/test_dsdgp.py
@@ -1,13 +1,14 @@
 import unittest
 
 import numpy as np
+import gpflow
 
 from dgplib.layers import InputLayer, OutputLayer, HiddenLayer
 from dgplib.models import Sequential
 
 from dgplib import DSDGP
 
-from gpflow.kernels import RBF
+from gpflow.kernels import RBF, White
 from gpflow.likelihoods import Gaussian
 from gpflow.mean_functions import Linear
 
@@ -39,3 +40,93 @@ class TestDSDGP(unittest.TestCase):
         except Exception as e:
             print(e)
             self.fail('DSDGP contructor fails')
+
+class TestMethods(unittest.TestCase):
+    def prepare(self):
+        N = 100
+        M = 10
+        rng = np.random.RandomState(42)
+        X = rng.randn(N, 2)
+        Y = rng.randn(N, 1)
+        Z = rng.randn(M, 2)
+        Xs = rng.randn(M, 2)
+        lik = Gaussian()
+        input_layer = InputLayer(input_dim=2, output_dim=1,
+                                 num_inducing=M, kernel=RBF(2)+White(2),
+                                 mean_function=Linear(A=np.ones((2,1))))
+        output_layer = OutputLayer(input_dim=1, output_dim=1,
+                                   num_inducing=M, kernel=RBF(1)+White(1))
+
+        seq = Sequential([input_layer, output_layer])
+
+        model = DSDGP(X=X, Y=Y, Z=Z, layers=seq, likelihood=lik)
+        model.compile()
+        return model, Xs
+
+    def test_build(self):
+        model, _ = self.prepare()
+        self.assertEqual(model.is_built_coherence(), gpflow.Build.YES)
+
+    def test_predict_f(self):
+        model, Xs = self.prepare()
+        mu, sigma = model.predict_f(Xs, 1)
+        with self.subTest():
+            self.assertEqual(mu.shape, sigma.shape)
+        with self.subTest():
+            self.assertEqual(mu.shape, (1, 10, 1))
+        with self.subTest():
+            np.testing.assert_array_less(np.full_like(sigma, -1e-6), sigma)
+
+    def test_predict_f_full_cov(self):
+        model, Xs = self.prepare()
+        mu, sigma = model.predict_f_full_cov(Xs)
+        with self.subTest():
+            self.assertEqual(mu.shape, (1, 10, 1))
+        with self.subTest():
+            self.assertEqual(sigma.shape, (1, 10, 10, 1))
+        with self.subTest():
+            np.testing.assert_array_less(np.full_like(sigma, -1e-6), sigma)
+
+    def test_predict_all_layers(self):
+        model, Xs = self.prepare()
+        fs, fmeans, fvars = model.predict_all_layers(Xs, 1)
+        dims = [1, 1]
+        for f, m, v, i in zip(fs, fmeans, fvars, dims):
+            with self.subTest():
+                self.assertEqual(m.shape, f.shape)
+            with self.subTest():
+                self.assertEqual(m.shape, v.shape)
+            with self.subTest():
+                self.assertEqual(m.shape, (1, 10, i))
+            with self.subTest():
+                np.testing.assert_array_less(np.full_like(v, -1e-6), v)
+
+    def test_predict_f_full_cov(self):
+        model, Xs = self.prepare()
+        fs, fmeans, fvars = model.predict_all_layers_full_cov(Xs, 1)
+        dims = [1, 1]
+        for f, m, v, i in zip(fs, fmeans, fvars, dims):
+            with self.subTest():
+                self.assertEqual(f.shape, (1, 10, i))
+            with self.subTest():
+                self.assertEqual(m.shape, (1, 10, i))
+            with self.subTest():
+                self.assertEqual(v.shape, (1, 10, 10, 1))
+            with self.subTest():
+                np.testing.assert_array_less(np.full_like(v, -1e-6), v)
+
+    def test_predict_f(self):
+        model, Xs = self.prepare()
+        fs = model.predict_f_samples(Xs, 10)
+        with self.subTest():
+            self.assertEqual(fs.shape, (10, 10, 1))
+
+    def test_predict_y(self):
+        model, Xs = self.prepare()
+        mu, sigma = model.predict_y(Xs)
+        with self.subTest():
+            self.assertEqual(mu.shape, sigma.shape)
+        with self.subTest():
+            self.assertEqual(mu.shape, (1, 10, 1))
+        with self.subTest():
+            np.testing.assert_array_less(np.full_like(sigma, -1e-6), sigma)
