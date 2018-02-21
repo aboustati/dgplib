b3ce4381b4bcab7154b57992b99e58918e8c548d
Ayman Boustati <ayman.boustati@outlook.com>
2018-02-21 11:10:25 +0000

    fixed error in __init__.py

--- dgplib/__init__.py
+++ dgplib/__init__.py
@@ -4,7 +4,7 @@ from . import layers
 from . import models
 from . import multikernel_layers
 from . import utils
-from . import specialized_kernels.py
+from . import specialized_kernels
 
 from .dsdgp import DSDGP
 
