30c77150b59f285a09d64ec4caa72cafc9b82286
Ayman Boustati <ayman.boustati@outlook.com>
2019-07-23 16:29:37 +0100

    clean up of repository in preparation for porting

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='dgplib',
-      version='0.3.1',
+setup(
+      name='dgplib',
+      version='0.5.0',
       author='Ayman Boustati',
-      author_email='aboustati@turing.ac.uk',
+      author_email='a.boustati@warwick.ac.uk',
       license='Apache License 2.0',
       packages=['dgplib'],
-      test_suite='tests.test_module_suite'
-     )
+      python_requires=">=3.7"
+)
